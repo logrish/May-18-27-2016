be00b4fbf54ac4d976b6e997d3b35af344136fcf
Admin <admin@Admins-MacBook-Pro.local>
2016-05-25 14:33:13 +0530

    Day06

--- webserver.js
+++ webserver.js
@@ -2,7 +2,10 @@ var http = require("http");
 var fs = require("fs");
 var server = http.createServer(function(request,response){
 	var url = request.url;
-	if(url == "/"){
+	if(url.match(/world/)){
+		getWorldInfo(request,response);
+	}
+	else if(url == "/"){
 		response.end("index.html not found");
 	}
 	else{
@@ -12,6 +15,18 @@ var server = http.createServer(function(request,response){
 		sendFile(fileName,response);
 	}
 });
+function getWorldInfo(request,response){
+	var queryString = require("url").parse(request.url,true).query;
+	var country = queryString.country;
+	var output = {"capital":"Not Available in DB","continent":"Not Available in DB"};
+	if(country == "India")
+		output = {"capital":"New Delhi","continent":"Asia"};
+	else if(country == "USA")
+		output = {"capital":"Washington DC","continent":"North America"};
+	else if(country == "UK")
+		output = {"capital":"London","continent":"Europe"};
+	response.end(JSON.stringify(output));
+}
 function sendFile(fileName,response){
 	fs.readFile(fileName,function(err,data){
 		if(err)
